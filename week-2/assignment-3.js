92b7063544a9fd8a391fe95c061d5f88c1419779
Sailwayfun <liaoleon000513@gmail.com>
2023-09-04 14:00:10 +0800

    try out week-2 assignment-3

---
+++ week-2/assignment-3.js
@@ -0,0 +1,15 @@
+function calculate(data) {
+
+}
+
+const discountedPrice = calculate({
+    discount: 0.1,
+    products: [
+        {name: "Product 1", price: 100},
+        {name: "Product 2", price: 700},
+        {name: "Product 3", price: 250}
+    ]
+
+});
+
+console.log(discountedPrice);
