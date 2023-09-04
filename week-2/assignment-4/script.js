af6d98bdf15dfc37d50f8ab2a89f81d44987ed7a
Sailwayfun <liaoleon000513@gmail.com>
2023-09-04 16:24:57 +0800

    finish week-2 assignment-4 request-2

--- week-2/assignment-4/script.js
+++ week-2/assignment-4/script.js
@@ -1,5 +1,31 @@
-document.querySelector("header").addEventListener("click", 
+document.querySelector("header").addEventListener("click",
     (event) => {
         document.querySelector("h1").innerText = "Have a Good Time!"
     }
 );
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const sideNavItem = document.querySelectorAll(".side-nav-item");
+const sideMenu = document.querySelector(".side-menu");
+const sideMenuToggle = document.querySelector(".side-menu-toggle");
+sideMenuToggle.addEventListener("click",
+    (event) => {
+        if (sideMenu.style.display === "block") {
+            sideMenu.style.display = "none";
+            hamburgerMenu.style.opacity = "1";
+        }
+    }
+);
+
+hamburgerMenu.addEventListener("click", (event) => {
+    if (hamburgerMenu.style.opacity === "1") {
+        hamburgerMenu.style.opacity = "0";
+        sideMenu.style.display = "block";
+    }
+    else {
+        hamburgerMenu.style.opacity = "1";
+        sideMenu.style.display = "none";
+    }
+
+
+})
