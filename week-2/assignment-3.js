1b56f51df457df53f891184f7219ec06751a84bf
Sailwayfun <liaoleon000513@gmail.com>
2023-09-08 22:15:28 +0800

    assignment 5 exclude a < b

--- week-2/assignment-3.js
+++ week-2/assignment-3.js
@@ -1,16 +1,21 @@
 function calculate(data) {
     const times = 1 - data.discount;
-    const priceArr = data.products.map(product => product.price * times);
-    const discountedAmount = priceArr.reduce((a, b) => a + b);
+    console.log(times);
+    // const priceArr = data.products.map(product => product.price * times);
+    // const discountedAmount = priceArr.reduce((a, b) => a + b);
+    const getTotalPrice = (prod1, prod2) => {
+        return (prod1.price * times + prod2.price * times);
+    }
+    const discountedAmount = data.products.reduce(getTotalPrice);
     return discountedAmount;
 }
 
 const discountedPrice = calculate({
     discount: 0.1,
     products: [
-        {name: "Product 1", price: 100},
-        {name: "Product 2", price: 700},
-        {name: "Product 3", price: 250}
+        { name: "Product 1", price: 100 },
+        { name: "Product 2", price: 700 },
+        { name: "Product 3", price: 250 }
     ]
 
 });
--- week-2/assignment-5.js
+++ week-2/assignment-5.js
@@ -5,11 +5,12 @@ function twoSum(nums, target) {
         a++;
         b = nums.findIndex(num => nums[a] + num === target);
     }
-    if (a > b) {
-        return [b, a];
-    } else {
-        return [a, b];
-    }
+    // if (a > b) {
+    //     return [b, a];
+    // } else {
+    //     return [a, b];
+    // }
+    return [a, b];//[0, n]不成立的話，[n, 0]也不成立，故可排除a < b
 
 
 }
