8ecaa50dd542c9e84f47f88b232ae9829ef8f9a9
Sailwayfun <liaoleon000513@gmail.com>
2023-09-12 21:36:59 +0800

    finish assignment-3

--- week-3/index.js
+++ week-3/index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
@@ -7,27 +8,28 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./index.html"));
 })
 
+///serve static files in "public" directory
+app.use(express.static("public"));
+
 ////the getData API
 app.get("/getData", (req, res) => {
     const query = req.query;
     const { number } = query;
     const num = Number(number);
     if (!number) {
-        res.send("Lack of Parameter");
+        res.send("<h1>Lack of Parameter</h1>");
     } else if (!Number.isInteger(num) || Number(num) < 1 || isNaN(Number(num))) {
-        res.send("Wrong Parameter");
+        res.send("<h1>Wrong Parameter</h1>");
     } else {
         const sum = ((1 + num) * (num) / 2).toString();
-        res.send(sum);
+        res.send(`<h1>${sum}</h1>`);
 
     }
     
 })
 
-////sum.html
-app.use("/sum.html", express.static(path.join(__dirname, "sum.html")));
 
-
+////listen to port 3000
 app.listen(port, () => {
     console.log(`Server is listening at port ${port}`);
 })
