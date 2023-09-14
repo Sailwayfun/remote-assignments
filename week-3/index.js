b97b72c00d86d0c7f91419c2a5ec0b2cb53c5061
Sailwayfun <liaoleon000513@gmail.com>
2023-09-14 15:57:51 +0800

    use modular routes

--- week-3/index.js
+++ week-3/index.js
@@ -4,29 +4,33 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./index.html"));
-})
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./index.html"));
+// })
+
+////use the route definitions
+const mainRoutes = require("./routes");
+app.use(mainRoutes);
 
 ///serve static files in "public" directory
 app.use(express.static("public"));
 
-////the getData API
-app.get("/getData", (req, res) => {
-    const query = req.query;
-    const { number } = query;
-    const num = Number(number);
-    if (!number) {
-        res.send("<h1>Lack of Parameter</h1>");
-    } else if (!Number.isInteger(num) || Number(num) < 1 || isNaN(Number(num))) {
-        res.send("<h1>Wrong Parameter</h1>");
-    } else {
-        const sum = ((1 + num) * (num) / 2).toString();
-        res.send(`<h1>${sum}</h1>`);
+// ////the getData API
+// app.get("/getData", (req, res) => {
+//     const query = req.query;
+//     const { number } = query;
+//     const num = Number(number);
+//     if (!number) {
+//         res.send("<h1>Lack of Parameter</h1>");
+//     } else if (!Number.isInteger(num) || Number(num) < 1 || isNaN(Number(num))) {
+//         res.send("<h1>Wrong Parameter</h1>");
+//     } else {
+//         const sum = ((1 + num) * (num) / 2).toString();
+//         res.send(`<h1>${sum}</h1>`);
 
-    }
+//     }
     
-})
+// })
 
 ///error handling--404
 app.use((req, res, next) => {
