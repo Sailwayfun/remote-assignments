734c8990a9127df7cb94172a26331ef6dab7383d
Sailwayfun <liaoleon000513@gmail.com>
2023-09-14 18:37:07 +0800

    finish adding cookie

--- week-3/index.js
+++ week-3/index.js
@@ -1,12 +1,16 @@
-const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./index.html"));
-// })
+///use Pug template
+app.set("view engine", "pug");
+
+///set up body parser and cookie parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 ////use the route definitions
 const mainRoutes = require("./routes");
@@ -14,23 +18,6 @@ app.use(mainRoutes);
 
 ///serve static files in "public" directory
 app.use(express.static("public"));
-
-// ////the getData API
-// app.get("/getData", (req, res) => {
-//     const query = req.query;
-//     const { number } = query;
-//     const num = Number(number);
-//     if (!number) {
-//         res.send("<h1>Lack of Parameter</h1>");
-//     } else if (!Number.isInteger(num) || Number(num) < 1 || isNaN(Number(num))) {
-//         res.send("<h1>Wrong Parameter</h1>");
-//     } else {
-//         const sum = ((1 + num) * (num) / 2).toString();
-//         res.send(`<h1>${sum}</h1>`);
-
-//     }
-    
-// })
 
 ///error handling--404
 app.use((req, res, next) => {
@@ -43,11 +30,11 @@ app.use((err, req, res, next) => {
     if (err.status === 404) {
         res.status(err.status).send(err.message);
     } else {
-        res.status = 500 || err.status;
         err.message = err.message || "There's something wrong with the server. Please connect again.";
+        res.status(500 || err.status);
         res.send(err.message);
     }
-}) 
+})
 
 ////listen to port 3000
 app.listen(port, () => {
--- week-3/routes/index.js
+++ week-3/routes/index.js
@@ -21,7 +21,35 @@ router.get("/getData", (req, res) => {
         res.send(`<h1>${sum}</h1><p>Refresh the page and try again.</p>`);
 
     }
-    
+
 })
+
+///an api to store cookie
+router.get("/myName", (req, res) => {
+    console.log(req.cookies.username);
+    if (req.cookies.username) {
+        res.render("my-name", { name: req.cookies.username });
+    } else {
+        res.redirect("/trackName");////if there areno username cookies,
+        //redirect the user to /trackName to fill out and submit the form
+    }
+
+});
+
+router.get("/trackName", (req, res) => {
+    if (req.cookies.username) {
+        return res.redirect("/myName");
+    } else {
+        res.render("my-name");
+    }
+})
+
+
+router.post("/trackName", (req, res) => {
+    const username = req.body.username;
+    res.cookie("username", username);
+    res.redirect(`/trackName?name=${username}`);
+})
+
 
 module.exports = router;
