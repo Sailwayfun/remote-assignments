a43528fb16887a0bbf1dcb7c9167946503e7744f
Sailwayfun <liaoleon000513@gmail.com>
2023-09-23 22:48:44 +0800

    add new workflow and adjust bases in vite config

--- week-4/assignment-5/react-counter/vite.config.js
+++ week-4/assignment-5/react-counter/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/remote-assignments/week-4/assignment-5/react-counter/dist"
 })
