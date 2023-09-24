ce00d26c094cfe428839feb37957938b16048657
Sailwayfun <liaoleon000513@gmail.com>
2023-09-24 10:09:39 +0800

    use React.memo to prevent wasted re-renders

--- week-4/assignment-5/react-counter/src/components/Counter.jsx
+++ week-4/assignment-5/react-counter/src/components/Counter.jsx
@@ -1,10 +1,18 @@
 import "../components/Counter.css";
-const Counter = (props) => {
+import { memo } from "react";
+const Counter = memo(function Counter(props) {
+    console.log(props.value, "rendered");
     const { value, onIncrement } = props;
     return <div className="counter-box">
         <button className="btn" onClick={onIncrement}>{props.children}</button>
         {props.children !== "All + 1" && props.children !== "Add Counter" && <div className="counter-result">{value ? value : 0}</div>}
     </div>
+}, compareProps);
+
+function compareProps (prevProps, nextProps) {
+    return prevProps.value === nextProps.value;
 }
+
+////use React.memo to prevent wasted re-renders
 
 export default Counter;
