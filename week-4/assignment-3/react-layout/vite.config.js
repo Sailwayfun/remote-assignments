d4afeacaa672d2a73db5e81b1d6374e130ed2141
Sailwayfun <liaoleon000513@gmail.com>
2023-09-24 17:19:52 +0800

    redeploy assignment-3

--- week-4/assignment-3/react-layout/vite.config.js
+++ week-4/assignment-3/react-layout/vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/remote-assignments/week-4/assignment-3/react-layout/dist"
 })
